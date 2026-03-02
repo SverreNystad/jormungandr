9466a59c1bbfa695f27aa2e5d50d68b5271a1c07
Sverre Nystad <sverrenystad@gmail.com>
2026-03-02 10:36:05 +0100

    refactor: make detr encoder test similar to mamba encoder

--- tests/test_detr_encoder.py
+++ tests/test_detr_encoder.py
@@ -24,3 +24,28 @@ def test_detr_encoder_inference(batch_size, sequence_length, model_dimension):
 
     assert y.shape == x.shape, f"Expected output shape {x.shape}, got {y.shape}"
     assert not y.equal(x), "Output should be different from input after encoding"
+
+
+@pytest.mark.skipif(
+    not torch.cuda.is_available(), reason="CUDA is required for this test"
+)
+def test_detr_encoder_inference_with_position_embedding():
+    weidth, height = 8, 8
+    sequence_length = weidth * height
+    batch_size, model_dimension = 2, 256
+    embedder_shape = (batch_size, 0, weidth, height)
+    x = torch.randn(batch_size, sequence_length, model_dimension).to("cuda")
+    
+    embedder = DetrSinePositionEmbedding(num_position_features=model_dimension // 2).to("cuda")
+    embedding = embedder(embedder_shape, device="cuda", dtype=x.dtype)
+    
+    encoder = DETREncoder(model_name="facebook/detr-resnet-50").to("cuda")
+
+    y_with_em = encoder(x, position_embedding=embedding)
+
+    assert y_with_em.shape == x.shape, (
+        f"Expected output shape {x.shape}, got {y_with_em.shape}"
+    )
+    assert not y_with_em.equal(x), (
+        "Output should be different from input after encoding with position embedding"
+    )
