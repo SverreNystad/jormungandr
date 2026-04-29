93dc969a0fa71aada39a736b654d83ebd73196c4
Sverre Nystad <sverrenystad@gmail.com>
2026-04-29 08:16:43 +0200

    refactor: restructure get_image_processor to use a global cache for image processor instance

--- src/jormungandr/datasets/processor.py
+++ src/jormungandr/datasets/processor.py
@@ -1,17 +1,21 @@
-from functools import cache
-
 from jormungandr.utils.image_processors import (
     DetrImageProcessorNoPadBBoxUpdate as DetrImageProcessor,
 )
 
+image_processor: DetrImageProcessor | None = None
 
-@cache
+
 def get_image_processor(
     model_name: str = "facebook/detr-resnet-50",
 ) -> DetrImageProcessor:
-    try:
-        return DetrImageProcessor.from_pretrained(model_name)
-    except Exception as e:
-        print(f"Error loading image processor from hub: {e}")
-        print("Attempting to load from local cache...")
-        return DetrImageProcessor.from_pretrained(model_name, local_files_only=True)
+    global image_processor
+    if image_processor is None:
+        try:
+            image_processor = DetrImageProcessor.from_pretrained(model_name)
+        except Exception as e:
+            print(f"Error loading image processor from hub: {e}")
+            print("Attempting to load from local cache...")
+            image_processor = DetrImageProcessor.from_pretrained(
+                model_name, local_files_only=True
+            )
+    return image_processor
